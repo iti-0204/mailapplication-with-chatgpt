462da07ca91e8ed9420c050c4d54a726125dbc56
iti-0204 <yasuken0327@icloud.com>
2023-11-20 01:47:30 +0900

    register,loginページ作成

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { auth } from "../../../../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { type } from "os";
+import { Input } from "postcss";
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const Login = () => {
+  const router = useRouter();
+  type Inputs = {
+    email: string;
+    password: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    await signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        router.push("/");
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-login-credentials") {
+          alert("そのようなユーザーは存在しません。");
+        } else {
+          console.log(error.message);
+        }
+      });
+  };
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-8 rounded-lg shadow-md w-96"
+      >
+        <h1 className="mb-4 text-2xl text-gray-700 font-medium">ログイン</h1>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
+          <input
+            {...register("email", {
+              required: "メールアドレスは必須です。",
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                message: "不適切なメールアドレスです",
+              },
+            })}
+            type="text"
+            className="mt-1 border-2 rounded-md w-full p-2"
+          />
+          {errors.email && (
+            <span className="text-red-600">{errors.email.message}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">
+            Password
+          </label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "パスワードは必須です。",
+              minLength: {
+                value: 6,
+                message: "6文字以上入力してください",
+              },
+            })}
+            className="mt-1 border-2 rounded-md w-full p-2"
+          />
+          {errors.password && (
+            <span className="text-red-600">{errors.password.message}</span>
+          )}
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
+          >
+            ログイン
+          </button>
+        </div>
+        <div className="mt-4">
+          <span className="text-gray-600 text-sm">
+            初めてのご利用の方はこちら
+          </span>
+          <Link
+            href={"/auth/register"}
+            className="text-blue-500 text-sm font-bold ml-l hover:text-blue-700"
+          >
+            　新規登録ページへ
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
